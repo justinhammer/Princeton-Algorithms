193a605e471d6453ff6a06a47fa6aa2995aa3a30
Justin Hammer <justhamr@gmail.com>
2017-06-15 22:00:05 -0600

    Getting Started

---
+++ PercolationStats.java
@@ -0,0 +1,14 @@
+public class PercolationStats {
+    public PercolationStats (int n, int trials){
+    }
+    public double mean(){
+    }
+    public double stddev(){
+    }
+    public double confidenceLo(){
+    }
+    public double confidenceHi(){
+    }
+    public static void main(String[] args){
+    }
+}
